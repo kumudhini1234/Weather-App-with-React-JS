721e4d37704633935fbe3a038c8acef8144fda3d
Kumudhini Reddicherla <kumudhini.reddicherla@gmail.com>
2024-10-29 15:57:39 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Weather from './components/Weather'
+
+// We are getting the component "Weather" here
+const App = () => {
+  return (
+    <div className='app'>
+      <Weather/> 
+    </div>
+  )
+}
+
+export default App
